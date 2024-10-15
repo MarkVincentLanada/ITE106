d5f43425bef66ef901d75d0175b628d8923fd173
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-10-15 09:58:59 +0800

    Update TextFileManipulationBufferedReader.java

--- TextFileManipulation/TextFileManipulationBufferedReader.java
+++ TextFileManipulation/TextFileManipulationBufferedReader.java
@@ -5,7 +5,7 @@
 import java.io.*;
  
 // Main class
-public class TextFileManipulationBufferedReader.java {
+public class TextFileManipulationBufferedReader {
  
     // main driver method
     public static void main(String[] args) throws Exception
