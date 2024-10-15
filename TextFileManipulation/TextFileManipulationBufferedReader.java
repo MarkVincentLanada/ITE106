b3e37b2a7338300b4ea51f04a09b3cba694f73f7
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-10-15 10:39:06 +0800

    Update TextFileManipulationBufferedReader.java

--- TextFileManipulation/TextFileManipulationBufferedReader.java
+++ TextFileManipulation/TextFileManipulationBufferedReader.java
@@ -1,33 +1,40 @@
-// Java Program to illustrate Reading from FileReader
-// using BufferedReader Class
- 
-// Importing input output classes
 import java.io.*;
- 
-// Main class
+/* ang gagamitin natin na CLASSES galing sa PACKAGE na java.io ay
+ * File at BufferedReader  
+ */
+
 public class TextFileManipulationBufferedReader {
  
-    // main driver method
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args) throws Exception // throws Exception - File handling errors/failures ipa handle na lang natin sa class kasi tamad tayo mag (try-catch)
     {
-        // File path is passed as parameter
-        File file = new File("C:\\Users\\pankaj\\Desktop\\test.txt");
-		
-        // Note:  Double backquote is to avoid compiler
-        // interpret words
-        // like \test as \t (ie. as a escape sequence)
- 
-        // Creating an object of BufferedReader class
-        BufferedReader br
-            = new BufferedReader(new FileReader(file));
- 
-        // Declaring a string variable
-        String st;
-        // Condition holds true till
-        // there is character in a string
-        while ((st = br.readLine()) != null)
- 
-            // Print the string
-            System.out.println(st);
+    	// ung file na 'Pogi Pogi.txt' ay hinahandle na ng 'balut'
+        FileReader balut = new FileReader("C:\\Users\\Mark Vincent\\Downloads\\Pogi Pogi.txt");
+        /* paltan niyo ung 'Mark Vincent' into -> user name ng pc na gamit niyo
+		 * punta lang kayo sa C:\\Users\\'User' para makita user name ng pc
+		 * 'C' ung disk, baka ibang letter ang nakalagay sa pc na gamit niyo
+		 * User - user name ng computer (account sa computer na gamit)
+		 */
+    	
+        /* ang 'BufferedReader' na CLASS ay ginamit ang 'FileReader' na class which is ung 'balut'
+         * dahil bawal ibigay direkta ang file na 'Pogi Pogi.txt' as ARGUMENT sa BufferedReader
+         * kaya ginagamit muna ang FileReader para ma convert ung bytes into characters 
+         */
+        BufferedReader kalbo = new BufferedReader(balut);
+        
+        /* 'kiwkiw' na variable ay mag ho hold ng line of string from the start hanggang may makitang '\n'
+         * ang BufferedReader gamit ang 'readLine' na method sa baba -
+         * ay binabasa ang file LINE BY LINE. ang dulo ng line ay pag may '\n'
+         */
+        String kiwkiw;
+        
+        /* dito ung 'readLine()' ay method ng FileReader na magbabalik ng line of string
+         * then ibibigay ung value na un sa 'kiwkiw' na variable
+         * then chicheck kung ung value ng 'kiwkiw' ay hindi equal sa 'null'
+         * ang 'null' ay END OF THE FILE
+         * in other words. kung wala pa sa dulo ng file ung 'kiwkiw' na variable ay
+         * dire-diretso lang ang 'while' loop 
+         */
+        while ((kiwkiw = kalbo.readLine()) != null)
+        	System.out.println(kiwkiw);
     }
 }
