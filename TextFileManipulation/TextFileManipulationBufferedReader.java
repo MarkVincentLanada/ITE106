36bc6bcd8d48839e275e4a9a84b0db492b2bda4a
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-10-15 09:58:50 +0800

    Update and rename 04TextFileManipulationBufferedReader.java to TextFileManipulationBufferedReader.java

--- TextFileManipulation/TextFileManipulationBufferedReader.java
+++ TextFileManipulation/TextFileManipulationBufferedReader.java
@@ -5,7 +5,7 @@
 import java.io.*;
  
 // Main class
-public class GFG {
+public class TextFileManipulationBufferedReader.java {
  
     // main driver method
     public static void main(String[] args) throws Exception
