a3422a1672ea9db87e4c97c9f6588bc6046161d6
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-11-08 15:11:32 +0800

    Update and rename Main.java to MultipleInheritance.java

--- InheritanceAndPolymorphism/MultipleInheritance.java
+++ InheritanceAndPolymorphism/MultipleInheritance.java
@@ -38,7 +38,7 @@ class BabyBulldog extends Bulldog {
 	}
 }
 
-public class Main {
+public class MultipleInheritance {
 	public static void main(String[] args) {
 		// Create an object of the BabyBulldog class
 		BabyBulldog babyBulldog = new BabyBulldog();
