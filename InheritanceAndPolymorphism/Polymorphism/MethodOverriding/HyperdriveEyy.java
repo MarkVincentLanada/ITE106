b9aa9a94971e28d48fd392bd60edac3c43934016
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-11-08 21:01:35 +0800

    Update HyperdriveEyy.java

--- InheritanceAndPolymorphism/Polymorphism/MethodOverriding/HyperdriveEyy.java
+++ InheritanceAndPolymorphism/Polymorphism/MethodOverriding/HyperdriveEyy.java
@@ -1,1 +1,25 @@
+// I will use inheritance to demonstrate method overriding
+class TopMostDude {
+	public void myMethod() {
+		System.out.println("Don't change my words bro!");
+	}
+}
 
+// LowerDude inherits all the attributes/variables and methods/functions of TopMostDude
+class LowerDude extends TopMostDude {
+	// overrides the 'myMethod' of TopMostDude
+	@Override // use this keyword to imply Method Overriding
+	public void myMethod() {
+		System.out.println("What you gonna do bout it?");
+	}
+}
+
+public class HyperdriveEyy {
+	public static void main(String[] args) {
+		TopMostDude theMan = new TopMostDude();
+		LowerDude theDude = new LowerDude();
+		
+		theMan.myMethod(); // calls original method
+		theDude.myMethod(); // calls overriden method
+	}
+}
