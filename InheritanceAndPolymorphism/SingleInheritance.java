f292d65873b0930dad65ca7d4acbee04d6dba712
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-11-08 15:19:38 +0800

    Create SingleInheritance.java

---
+++ InheritanceAndPolymorphism/SingleInheritance.java
@@ -0,0 +1,34 @@
+
+// Level 1: Base Class/Super Class
+class Father {
+	String Features = "Pogi";
+	String EyeColor = "Green";
+	int Money = 612879;
+	
+	public printFatherFeatures() {
+		System.out.println(Features);
+		System.out.println(EyeColor);
+		System.out.println(Money);
+	}
+}
+
+// Level 2: Derived Class (Subclass of Animal, Parent Class of Dog)
+class Son extends Father {
+	String Features = super.Features;
+	String EyeColor = super.EyeColor;
+	int Money = super.Money / 100;
+	
+	public printSonFeatures() {
+		System.out.println(Features);
+		System.out.println(EyeColor);
+		System.out.println(Money);
+	}
+}
+
+public class SingleInheritance {
+	public static void main(String[] args) {
+		Son rupart;
+		rupart.printFatherFeatures();
+		rupart.printSonFeatures();
+	}
+}
