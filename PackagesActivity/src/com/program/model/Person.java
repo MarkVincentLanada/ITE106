2e131b5dca7b9e4c0cf9655f77f8fc0b155743a7
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-11-08 10:48:18 +0800

    Update Person.java

--- PackagesActivity/src/com/program/model/Person.java
+++ PackagesActivity/src/com/program/model/Person.java
@@ -1,18 +1,21 @@
+// part of the 'program' package
 package com.example.model;
 
 public class Person {
-    private String name;
+    // cannot be access 
+    private String name
     private int age;
-
+    
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+    // to get/retrieve variables/attributes of INSTANTIATED OBJECTS, not this CLASS
     public String getName() {
         return name;
     }
-
+    
     public int getAge() {
         return age;
     }
