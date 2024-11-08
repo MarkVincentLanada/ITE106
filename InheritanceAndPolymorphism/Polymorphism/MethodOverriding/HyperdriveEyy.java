3b4accab2d20586e1b170cb05fb0da609da9387f
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-11-08 21:09:34 +0800

    Update HyperdriveEyy.java

--- InheritanceAndPolymorphism/Polymorphism/MethodOverriding/HyperdriveEyy.java
+++ InheritanceAndPolymorphism/Polymorphism/MethodOverriding/HyperdriveEyy.java
@@ -1,7 +1,8 @@
 // I will use inheritance to demonstrate method overriding
 class TopMostDude {
-	public void myMethod() {
-		System.out.println("Don't change my words bro!");
+	// you don't necessarily need a parameter for the method
+	public void myMethod(String randomText) {
+		System.out.println("Don't change my words bro! (" + randomText + ")");
 	}
 }
 
@@ -9,8 +10,17 @@ class TopMostDude {
 class LowerDude extends TopMostDude {
 	// overrides the 'myMethod' of TopMostDude
 	@Override // use this keyword to imply Method Overriding
-	public void myMethod() {
-		System.out.println("What you gonna do bout it?");
+	public void myMethod(String randomText) {
+		System.out.println("What you gonna do bout it? (" + randomText + ")");
+	}
+}
+
+// LowerDude inherits all the attributes/variables and methods/functions of LowerDude and TopMostDude
+class RandomDude extends LowerDude {
+	// overrides the 'myMethod' of LowerDude
+	@Override // use this keyword to imply Method Overriding
+	public void myMethod(String randomText) {
+		System.out.println("Whatchu goyz doin'? (" + randomText + ")");
 	}
 }
 
@@ -18,8 +28,10 @@ public class HyperdriveEyy {
 	public static void main(String[] args) {
 		TopMostDude theMan = new TopMostDude();
 		LowerDude theDude = new LowerDude();
+		RandomDude thePasserby = new RandomDude();
 		
-		theMan.myMethod(); // calls original method
-		theDude.myMethod(); // calls overriden method
+		theMan.myMethod("hatdog"); // calls original method
+		theDude.myMethod("hehe"); // calls overriden method
+		thePasserby.myMethod("oh wow"); // calls second overriden method
 	}
 }
