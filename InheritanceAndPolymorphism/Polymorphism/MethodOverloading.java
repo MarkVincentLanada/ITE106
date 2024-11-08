1bb3868825ed406856cdc1a14339f08ad3f80ae6
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-11-08 20:41:07 +0800

    Update MethodOverloading.java

--- InheritanceAndPolymorphism/Polymorphism/MethodOverloading.java
+++ InheritanceAndPolymorphism/Polymorphism/MethodOverloading.java
@@ -1,5 +1,12 @@
+package project;
+
 public class MethodOverloading {
 	public static void main(String[] args) {
-		
+		OverloadedClass OC = new OverloadedClass();
+		OC.giveValue(true);
+		OC.giveValue("HOTDOG");
+		OC.giveValue(123456890);
+		OC.giveValue('A');
+		OC.giveValue(12.81f);
 	}
 }
