0bcf54a493604412264c8e2336f15096db7073a1
MarkVincentLanada <markvincentdesacola@gmail.com>
2024-11-08 10:56:45 +0800

    Update App.java

--- PackagesActivity/src/com/program/app/App.java
+++ PackagesActivity/src/com/program/app/App.java
@@ -1,11 +1,15 @@
+// part of the 'program' package
 package com.program.app;
 
+// to use/access variables/attributes and methods/functions of the Class 'Helper'
 import com.program.utils.Helper;
-import com.program.model.Person;
 
 public class App {
+    // built-in method, not user-defined
     public static void main(String[] args) {
+        // new object 'person' instantiated from the Class 'Person'. Not passed as argument because 'main' method has it's own parameter which is 'String[]'
         Person person = new Person("Alice", 30);
+        // uses the 'printPersonInfo()' method of the class 'Person' using the object 'person'
         Helper.printPersonInfo(person);
     }
 }
